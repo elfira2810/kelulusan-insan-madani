d2e572e67d90b9dd0535f55a3fd4332a8c036712
elfira2810 <elfirafauzia28@gmail.com>
2025-06-02 15:03:01 +0700

    data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const students = [
+  { nisn: "0094042958", name: "Adinda Hasna Juneeta", birthplace: "Bandung", birthdate: "2009-08-15", status: "LULUS" },
+  { nisn: "0096077793", name: "Aditya Wicaksono", birthplace: "Kab. Semarang", birthdate: "2009-12-30", status: "LULUS" },
+  { nisn: "0106923681", name: "Adnan Ibadurrohman", birthplace: "Semarang", birthdate: "2010-01-01", status: "LULUS" },
+  { nisn: "3103278945", name: "Ahmad Thoriq Al-Faiz", birthplace: "Semarang", birthdate: "2010-10-04", status: "LULUS" },
+  { nisn: "0097827195", name: "Anugerah Cahya Rezki", birthplace: "Karanganyar", birthdate: "2009-11-02", status: "LULUS" },
+  { nisn: "0101591560", name: "Ardiansyah Septian Angga Saputra", birthplace: "Batang", birthdate: "2010-09-10", status: "LULUS" },
+  { nisn: "0115645915", name: "Bunga Prameswari Ekila Aleyfanda", birthplace: "Semarang", birthdate: "2011-01-04", status: "LULUS" },
+  { nisn: "0097959605", name: "Farzal Qutb Abdurrahman", birthplace: "Semarang", birthdate: "2009-07-01", status: "LULUS" },
+  { nisn: "0095179612", name: "Fathu Yaisy Muhammad Ramadhan", birthplace: "Sungai Penuh", birthdate: "2009-09-05", status: "LULUS" },
+  { nisn: "0101720313", name: "Ghozi Taufiqurrohman", birthplace: "Jepara", birthdate: "2010-05-29", status: "LULUS" }
+];
